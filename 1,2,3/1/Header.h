563c96810331e8024a341b274c08af682380ee40
New-Skytech <76553267+New-Skytech@users.noreply.github.com>
2023-02-24 14:06:46 +0500

    Add files via upload

---
+++ 1,2,3/1/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+int test1 = 10;
+
+static int test2 = 15;
+
+int test3_1 = 9;
+int test3_2 = 11;
+
+void print(int text) {
+	std::cout << text << std::endl;
+}
---
+++ 1,2,3/1/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Header.h"
+
+int main() {
+
+	std::cout << test1 << std::endl;
+	print(test2);
+
+	test3_1 += 11;
+	test3_2 += 9;
+	print(test3_1);
+	print(test3_2);
+}
